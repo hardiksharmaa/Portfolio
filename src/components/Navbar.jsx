9816d07662149b9399ce9e5ce6c4f7e6267fe81d
Hardik <hs489819@gmail.com>
2025-11-19 20:50:49 +0530

    navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,8 +25,8 @@ export const Navbar = () => {
   return (
     <nav
       className={cn(
-        "fixed w-full z-40 transition-all duration-300",
-        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+        "fixed w-full z-40 transition-all duration-300 mt-5"
+        // isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
       )}
     >
       <div className="container flex items-center justify-between">
