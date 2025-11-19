f7c77e9446f0e3cfe4d3f739ad9e3d363e6e709c
Hardik <hs489819@gmail.com>
2025-11-19 12:03:40 +0530

    about-section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
 import { Navbar } from "../components/Navbar";
 import {HeroSection} from "../components/HeroSection";
+import {AboutSection} from "../components/AboutSection";
 
 export const Home = () => {
   return (
@@ -12,6 +13,8 @@ export const Home = () => {
       <main>
         <HeroSection/>
       </main>
+      <AboutSection/>
+
 
     </div>
   );
